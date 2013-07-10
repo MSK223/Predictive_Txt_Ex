04a17d8dc683c376ae26efc830cf052e59907afe
nyghtowl <warrick.melanie@gmail.com>
2013-07-10 14:30:55 -0700

    Revised variables and commentsto provide clarity.

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-from model import parse_file
+from model import build_model
 
 '''
 Generate a sorted list of predictions.  
@@ -7,16 +7,17 @@ Generate a sorted list of predictions.
 
 def predict(so_far, num_predictions=10):
 
-    prediction_list = [] #List to capture pred words & frequencies.
-    pred_word_list = [] #Just prediciton words.
+    prediction_list = [] #List to capture pred words & frequencies
+    pred_word_list = [] #Just prediciton words
     counter = 0
 
-    #Create model trie.
-    word_trie = parse_file("data.txt")
+    #Create trie model
+    word_trie = build_model("data.txt")
 
+    #Returns sorted list of tuples based on text typed so_far
     result = sorted(word_trie.search(so_far, prediction_list), key=lambda tup: int(tup[1]), reverse = True)
 
-    #Strips out frequences from prediction list.
+    #Strips out frequences from prediction list
     for word in result:
         if counter < num_predictions:
             pred_word_list.append(word[0])
